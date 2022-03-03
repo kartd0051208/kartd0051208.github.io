e6856352ee5b50b8ce9d13e836fb87f1b5419e00
Eli YE <inhibitedman@gmail.com>
2022-03-03 22:18:28 +0800

    test

---
+++ Javascript/Jacascript練習/javascript/main.js
@@ -0,0 +1,15 @@
+// const myLove = document.querySelector("a");
+// console.log(myLove);
+
+// myLove.setAttribute("href","https://www.youtube.com/channel/UCEQIhtDgkJbm_7r9ftRiDSg/videos");
+// myLove.setAttribute("class","red");
+// 
+
+// const myLove = document.querySelectorAll("a");
+// console.log(myLove);
+// 
+// myLove[0].setAttribute("class","red");
+// myLove[1].setAttribute("href","https://docs.google.com/document/d/13nK_XY9u5uIleTpSCw88lMupzgCSwXd6j6je44eLhMQ/edit");
+// 
+
+
